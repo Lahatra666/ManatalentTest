1da4b722f44c03e3a2995686bb1e497c751649b6
Lahatra Rabe <rabenarivolahatra@gmail.com>
2024-11-10 12:04:38 +0300

    [ALL] add .env configuration #main

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,16 +8,20 @@ import { UserModule } from './user/user.module';
 import { User } from './user/entities/user.entity';
 import { UserEntityModule } from './user-entity/user-entity.module';
 import { UserEntity } from './user-entity/entities/user-entity.entity';
+import * as dotenv from 'dotenv';
+import * as env from 'env-var';
+
+dotenv.config({ path: '.env' });
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'mana',
+      host: env.get('MYSQL_HOST').required().asString(),
+      port: env.get('MYSQL_PORT').required().asPortNumber(),
+      username: env.get('MYSQL_USER').required().asString(),
+      password: env.get('MYSQL_PASSWORD').asString(),
+      database: env.get('MYSQL_DATABASE_NAME').required().asString(),
       entities: [Entitiy,User,UserEntity],
       synchronize: true,
     }),
